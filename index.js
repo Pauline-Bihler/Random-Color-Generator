3b60311cba3277b841e9665aef9732f4fbbcb45f
Pauline Bihler <pauline.erika5@gmail.com>
2023-09-06 17:47:47 +0200

    Add some changes to the code

--- index.js
+++ index.js
@@ -15,37 +15,3 @@ console.log(
 ###############################
 ###############################`),
 );
-
-// Code with a box and hex code inside with color. Different code from the one above.
-
-function genColor() {
-  return randomColor();
-}
-
-let totalColumn = 31;
-let totalRow = 9;
-let string = '';
-
-for (let row = 1; row <= totalRow; row++) {
-  for (let column = 1; column <= totalColumn; column++) {
-    if (row === 4 || row === 6) {
-      if (column === 1 || column === 22) {
-        string += '#####';
-      }
-      string += ' ';
-    } else if (row === 5) {
-      if (column === 7) {
-        string += genColor();
-      } else if (column === 1 || column === 15) {
-        string += '#####';
-      }
-      string += ' ';
-    } else if (true) {
-      string += '#';
-    }
-  }
-  string += '\n';
-}
-console.log('\n');
-
-console.log(chalk.hex(color).bold(string));
